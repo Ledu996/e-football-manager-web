c83ec4c52aad71b0c2958fc8f7a98cde124be619
Ledu996 <dusan.dorcol@hotmail.com>
2021-11-22 15:08:55 +0100

    Adds Landing page

---
+++ src/Components/Button/Index.js
@@ -0,0 +1,12 @@
+import './index.css';
+import React from 'react';
+
+
+
+const Button = ({title}) => {
+    return (
+        <button>{title}</button>
+    )
+}
+
+export default  Button;
---
+++ src/Components/Header/index.js
@@ -0,0 +1,19 @@
+
+
+
+
+
+function Header () {
+    return (
+        <div className ="header-wrapper">
+            <ul>
+                <li>Home</li>
+                <li>Login</li>
+                <li>Sing Up</li>
+            </ul>
+        </div>
+    )
+}
+
+
+export default Header;
